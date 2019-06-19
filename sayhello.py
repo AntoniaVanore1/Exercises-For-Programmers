dbfae85336eb0279dbb6391f7cb20b4567ae7695
Antonia Cassida <Antonia@Antonias-MacBook.local>
2019-06-19 13:21:11 -0500

    Initial Commit

---
+++ sayhello.py
@@ -0,0 +1,14 @@
+# 1st Exercise is a variation on "Hello, World", except for the 
+# addition of input.
+
+# First, create your variable and get the input
+
+name = input("What is your name? ")
+
+# I'm assuming Python 3.7, if you are using 2, you need to 
+# use raw_input() - input() in Python 2 attempts to evaluate the given
+# data, while raw_input() will merely accept what the user types as a string
+# Next, you need to print the message:
+
+print("Hello, " + name + ", nice to meet you!")
+
